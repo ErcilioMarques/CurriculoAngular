d5f61d448038fadef2add53a92eb31f9d265cae7
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-12 04:34:17 +0200

    all necessary components installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    ExperienciaComponent,
+    PerfilComponent,
+    ContactoComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/experiencia/experiencia.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExperienciaComponent } from './experiencia.component';
+
+describe('ExperienciaComponent', () => {
+  let component: ExperienciaComponent;
+  let fixture: ComponentFixture<ExperienciaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExperienciaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExperienciaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
